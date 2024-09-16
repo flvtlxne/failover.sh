eed8ad814e99644ce1d4f9906288e8988ccb6ecf
Oleg Wiessmann <85104694+flvtlxne@users.noreply.github.com>
2024-09-16 10:00:17 +0300

    Update failover.sh

--- failover.sh
+++ failover.sh
@@ -14,12 +14,8 @@ else
     if [[ $interf == *"eno2"* ]]; then #if eno1 is default interface, so we need to change it to eno2 with failover provider
     /usr/sbin/ifup eno1 && sleep 5 #waking up the first interface
     echo "[$]$hos eno1 is our primary interface now $dat $interf" >> /var/log/failover/failover$dat.log
-    curl -X POST -H "Content-type: application/json" -d '{ "text" "'$hos' Primary provider is now OBIT" }' 'https://hooks.slack.com/services/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'
-    bash /opt/scripting/telegram 'Primary provider is now OBIT'
     else
     /usr/sbin/ifup eno2 && sleep 5 #waking up the second interface
     echo "[$]$hos eno2 is our primary interface now $dat $interf" >> /var/log/failover/failover$dat.log
-    curl -X POST -H "Content-type: application/json" -d '{ "text" "'$hos' Primary provider is now GIS" }' 'https://hooks.slack.com/services/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'
-    bash /opt/scripting/telegram 'Primary provider is now GIS'
     fi
 fi
